aadfc60ce56af9140338ba2b4551eacdb9629971
Karla Hugo <karlabrandon817@gmail.com>
2016-10-28 19:59:24 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var greeting = function (name) {
+  console.log ("Hey, " + name + ", this works!");
+}
+greeting("Tau Instructors");
+
+var buttonClick = function() {
+  alert ("Thanks for stopping by!")
+};
